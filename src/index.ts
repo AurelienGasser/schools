d8579ab56488c14372cc8453b02c0bb69807fda1
Aurélien Gasser <aurelien.gasser@gmail.com>
2026-08-21 15:22:56 -0400

    wip

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,10 @@
-import * as schools from "../data/schools.json";
-// import * as ppublic from "../data/public.json";
-// import * as pprivate from "../data/private.json";
-// import * as charter from "../data/charter.json";
+import type { SchoolsResponse } from "./types.js";
+import schoolsJson from "../data/schools.json" with { type: "json" };
+const schools = schoolsJson as unknown as SchoolsResponse;
 
-const schoolsByName = schools.default.features.map((s) => [
-  s.attributes.LEGAL_NAME,
-]);
-
-const find = (src: any[]) => {
-  const schoolsByName = new Map(
-    schools.default.features.map((s) => [s.attributes.LEGAL_NAME, s]),
-  );
-  console.log(src.length, schoolsByName.size);
-  for (const s of src) {
-  }
-};
-
-// find(schools.default.features);
-// find(ppublic.default.features);
-// find(pprivate.default.features);
-// find(charter.default.features);
-
-console.log(schools.default.features.length);
+console.log(schools.features.length);
 const byType = Map.groupBy(
-  schools.default.features,
+  schools.features,
   (s) => s.attributes.RECORD_TYPE_DESC,
 );
 for (const [type, entries] of byType) {
