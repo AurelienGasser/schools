b7688ad90a12368f8d68259e29976cca153733b8
Aurélien Gasser <aurelien.gasser@gmail.com>
2026-08-21 15:20:37 -0400

    wip

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,31 @@
-console.log("Hello, world!");
+import * as schools from "../data/schools.json";
+// import * as ppublic from "../data/public.json";
+// import * as pprivate from "../data/private.json";
+// import * as charter from "../data/charter.json";
+
+const schoolsByName = schools.default.features.map((s) => [
+  s.attributes.LEGAL_NAME,
+]);
+
+const find = (src: any[]) => {
+  const schoolsByName = new Map(
+    schools.default.features.map((s) => [s.attributes.LEGAL_NAME, s]),
+  );
+  console.log(src.length, schoolsByName.size);
+  for (const s of src) {
+  }
+};
+
+// find(schools.default.features);
+// find(ppublic.default.features);
+// find(pprivate.default.features);
+// find(charter.default.features);
+
+console.log(schools.default.features.length);
+const byType = Map.groupBy(
+  schools.default.features,
+  (s) => s.attributes.RECORD_TYPE_DESC,
+);
+for (const [type, entries] of byType) {
+  console.log(`${type}: ${entries.length}`);
+}
---
+++ src/types.ts
@@ -0,0 +1,64 @@
+export type EsriFieldType =
+  | "esriFieldTypeOID"
+  | "esriFieldTypeString"
+  | "esriFieldTypeDouble"
+  | "esriFieldTypeInteger"
+  | "esriFieldTypeDate";
+
+export interface EsriField {
+  name: string;
+  type: EsriFieldType;
+  alias: string;
+  sqlType: string;
+  length?: number;
+  domain: null;
+  defaultValue: null;
+}
+
+export interface SchoolAttributes {
+  OBJECTID: number;
+  LEGAL_NAME: string;
+  PHYSADDRLINE1: string;
+  PHYSADDRLINE2: string;
+  PHYSCITY: string;
+  PHYSICALSTATE: string;
+  PHYSZIPCD5: string;
+  COUNTY_DESC: string;
+  Contact_Name: string;
+  CEO_TITLE: string;
+  CEO_PHONENUM: string;
+  CEO_EMAIL: string;
+  INST_TYPE_DESC: string;
+  INSTSUBTYPDESC: string;
+  RECORD_TYPE_DESC: string;
+  COMMUNITY_TYPE_DESC: string;
+  DIST_TYPE_DESC: string;
+  SDL_DESC: string;
+  INSTIT_ID: number;
+  SED_CODE: string;
+}
+
+export interface SchoolFeature {
+  attributes: SchoolAttributes;
+  geometry: {
+    x: number;
+    y: number;
+  };
+}
+
+export interface SchoolsResponse {
+  objectIdFieldName: string;
+  uniqueIdField: {
+    name: string;
+    isSystemMaintained: boolean;
+  };
+  globalIdFieldName: string;
+  geometryType: string;
+  spatialReference: {
+    wkid: number;
+    latestWkid: number;
+  };
+  fields: EsriField[];
+  exceededTransferLimit: boolean;
+  features: SchoolFeature[];
+}
